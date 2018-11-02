f53c6d106c968c3c6c40f1f4da6561153661be1d
Podgorskiy <stanislav@podgorskiy.com>
2018-11-02 15:06:40 -0400

    Added more examples. Bumped version to 0.11

---
+++ example/basic.py
@@ -0,0 +1,19 @@
+import bimpy
+
+ctx = bimpy.Context()
+
+ctx.init(600, 600, "Hello")
+
+str = bimpy.String()
+f = bimpy.Float();
+
+while(not ctx.should_close()):
+    with ctx:
+        bimpy.text("Hello, world!")
+
+        if bimpy.button("OK"):
+            print(str.value)
+
+        bimpy.input_text('string', str, 256)
+
+        bimpy.slider_float("float", f, 0.0, 1.0)
