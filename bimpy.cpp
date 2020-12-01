7858cec28046c34b1a60aecdeca6afea6df14bde
Stanislav Pidhorskyi <stanislav@podgorskiy.com>
2020-12-01 02:29:11 -0500

    Reverted to the old glfw and opengl3 implementation, since the new one gives segfaults for unknown reason

--- bimpy.cpp
+++ bimpy.cpp
@@ -8,6 +8,7 @@
 #include <imgui_internal.h>
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
+#include "runtime_error.h"
 #include <GL/gl3w.h>
 #include <GLFW/glfw3.h>
 #include <pybind11/pybind11.h>
